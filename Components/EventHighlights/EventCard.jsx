8ecf99dbea0432fada465b37a60e476654438170
BARdANKARKI118 <bardankarki5@gmail.com>
2025-12-19 15:56:54 +0545

    Event

--- Components/EventHighlights/EventCard.jsx
+++ Components/EventHighlights/EventCard.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "./EventCard.css"
 
-const EventCard = ({icon,heading,msg,photo,}) => {
+const EventCard = ({ icon, heading, msg, photo }) => {
   return (
-    <div>
-        <div>{icon}</div>
-        <h1>{heading}</h1>
-        <p>{msg}</p>
-        <img src="{photo}" alt="" />
+    <div className='eventstyle'>
+      <div className='iconstyle'>{icon}</div>
+      <h1 className='textstyle'>{heading}</h1>
+      <p className='loremstyle'>{msg}</p>
+      {photo && (
+        <img
+          src={photo}
+          className="event-image"
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
--- Components/EventHighlights/EventHighlights.jsx
+++ Components/EventHighlights/EventHighlights.jsx
@@ -1,35 +1,62 @@
-import React from 'react'
-import "./EventHighlights.css"
-import EventCard from './EventCard'
-import { Icon } from '@iconify/react'
+import React from "react";
+import "./EventHighlights.css";
+import EventCard from "./EventCard";
+import { Icon } from "@iconify/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const EventHighlights = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   return (
     <div>
-        <h1 className='eventText'>Our  <span className='eventColor'>Event  Highlights</span></h1>
-        <div className='eventLine'></div>
+      <h1 className="eventText">
+        Our <span className="eventColor">Event Highlights</span>
+      </h1>
+      <div className="eventLine"></div>
 
-        <div className='event-container'>
-            <EventCard 
-            icon={<Icon icon="jam:mic"/>}
+      <div className="event-container">
+        <Slider {...settings}>
+          <EventCard
+            className="EventCard1"
+            icon={<Icon className="mic" icon="solar:podcast-bold" />}
             heading="Our Podcast"
-            msg="If you are passionate about helping us achieve our goal to deliver meals seamlessly, come join the team."
-            />
+            msg="An engaging podcast with CTO & Founder Saroj Dahal. Exploring innovation, startups, and technology leadership. A conversation filled with inspiration and practical knowledge."
+            photo="../public/images/span.png"
+          />
 
-            <EventCard
-            icon={<Icon icon="cib:hackster"/>}
+          <EventCard
+            className="EventCard2"
+            icon={<Icon className="hackathon" icon="cib:hackster" />}
             heading="Nepathon by Fusion Stack"
-            msg="If you are passionate about helping us achieve our goal to deliver meals seamlessly, come join the team."
-            />
+            msg="Nepathya Media proudly supported Nepathon organized by Fusion Stack. We contributed through media coverage and creative promotion. The event encouraged innovation and student collaboration."
+            photo="../public/images/span2.png"
+          />
 
-            <EventCard
-            icon={<Icon icon="jam:calendar"/>}
+          <EventCard
+            className="EventCard3"
+            icon={<Icon className="calendar" icon="jam:calendar" />}
             heading="Nepathya Annual Function"
-            msg="If you are passionate about helping us achieve our goal to deliver meals seamlessly, come join the team."
-            />
-        </div>
-    </div> 
+            msg="Nepathya Media captured the highlights of the Nepathya Annual Function.Showcasing performances, awards, and memorable moments of the event. Celebrating creativity, talent, and community in one grand program."
+            photo="../public/images/span3.png"
+          />
 
-  )
-}
+          <EventCard
+            className="EventCard4"
+            icon={<Icon className="dance" icon="jam:backward" />}
+            heading="Pabson Dance Competition"
+            msg="We successfully managed media coverage for the Pabson Dance Competition. Highlighting performances, excitement, and memorable moments. Preserving the celebration of creativity and passion in dance."
+            photo="../public/images/span4.png"
+          />
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
-export default EventHighlights
+export default EventHighlights;
